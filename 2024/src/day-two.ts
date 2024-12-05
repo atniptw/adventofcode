c08b528e299b8fee4f19e9eaf42d55914ed890f5
Tom Atnip <atnip@sep.com>
2024-12-05 12:50:40 -0500

    Day Two Solution

---
+++ 2024/src/day-two.ts
@@ -0,0 +1,38 @@
+import readline from 'readline'
+
+function isDescending(a: number[]): boolean {
+    return a.every((currentValue, index) => {
+        return index === 0 || (currentValue < a[index - 1] && Math.abs(currentValue - a[index - 1]) < 4);
+    });
+}
+
+function isAscending(a:number[]): boolean {
+    return a.every((currentValue, index) => {
+        return index === 0 || (currentValue > a[index - 1] && Math.abs(currentValue - a[index - 1]) < 4);
+    });
+}
+
+export function isSafe(a: number[]): boolean {
+    return isDescending(a) || isAscending(a);
+}
+
+export function isSafeEnough(a: number[]): boolean {
+    return isSafe(a) || a.some((v, i, arr) => {
+        const clone = [...arr];
+        clone.splice(i,1);
+        return isSafe(clone);
+    })
+}
+
+export function dayTwo(reader: readline.Interface) {
+    const reports: number[][] = [];
+
+    reader.on('line', (line) => {
+      const reportRaw = line.split(' ');
+      reports.push(reportRaw.map(i => parseInt(i)));
+    })
+      .on('close', () => {
+        console.log(`Day Two Part One: ${reports.filter(report => isSafe(report)).length}`);
+        console.log(`Day One Part Two: ${reports.filter(report => isSafeEnough(report)).length}`);
+      });  
+}
--- 2024/src/index.ts
+++ 2024/src/index.ts
@@ -1,10 +1,12 @@
 import fs from 'node:fs';
 import readline from 'readline'
-import { dayOne } from './day-one';
+// import { dayOne } from './day-one';
+import { dayTwo } from './day-two';
 
 const reader = readline.createInterface({
   input: fs.createReadStream('./src/input.txt'),
   crlfDelay: Infinity
 });
 
-dayOne(reader);
+// dayOne(reader);
+dayTwo(reader);
